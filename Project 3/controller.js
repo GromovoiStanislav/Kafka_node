f857fa7416c5e2ce5cc73d4c6c4512f04dee1812
Gromovoi Stanislav <gromila12@rambler.ru>
2023-09-14 16:44:07 +0600

    Project 3

--- Project 3/controller.js
+++ Project 3/controller.js
@@ -1,13 +1,12 @@
 import KafkaConfig from './kafka.js';
 
-const kafkaConfig = new KafkaConfig();
-
 const sendMessageToKafka = async (req, res) => {
   try {
     const { message } = req.body;
 
+    const kafkaConfig = new KafkaConfig();
     const messages = [{ key: 'key1', value: message }];
-    await kafkaConfig.produce('my-topic', messages);
+    kafkaConfig.produce('my-topic', messages);
 
     res.status(200).json({
       status: 'Ok!',
