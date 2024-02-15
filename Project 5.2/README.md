9f0345d6c2dfc547db3f412dca4629466dcb7eba
Gromovoi Stanislav <gromila12@rambler.ru>
2024-02-15 11:38:06 +0600

    Project 5.2

---
+++ Project 5.2/README.md
@@ -0,0 +1,10 @@
+## Kafka example with different partitions + admin topic (JS)
+
+```
+npm run admin
+
+npm run consumer
+
+npm run producer Hello
+npm run producer World
+```
---
+++ Project 5.2/producer.js
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import { Kafka, Partitioners } from 'kafkajs';
+
+async function produce() {
+  const kafka = new Kafka({
+    brokers: [process.env.KAFKA_HOSTNAME],
+    sasl: {
+      mechanism: 'scram-sha-256',
+      username: process.env.KAFKA_USERNAME,
+      password: process.env.KAFKA_PASSWORD,
+    },
+    ssl: true,
+  });
+
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner, // Используем старый разделитель
+  });
+
+  await producer.connect();
+  console.log('Producer connected');
+
+  const msg = process.argv[2];
+  //A-M 0 , N-Z 1
+  const partition = msg[0] < 'N' ? 0 : 1;
+  const result = await producer.send({
+    topic: 'Users',
+    messages: [
+      {
+        value: msg,
+        partition: partition,
+      },
+    ],
+  });
+
+  console.log(`Send Successfully! ${JSON.stringify(result)}`);
+  await producer.disconnect();
+
+  // const producedData = await producer.send({
+  //   topic: 'my-topic',
+  //   messages: [
+  //     {
+  //       value: players[jerseyNumber],
+  //       partition: jerseyNumber <= 10 ? 0 : 1,
+  //     },
+  //   ],
+  // });
+  // console.log(`Produced data ${JSON.stringify(producedData)}`);
+
+  //setTimeout(producer.disconnect, 500);
+}
+
+produce();
